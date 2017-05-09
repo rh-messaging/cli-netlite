5dd1dfb9bcd381c8bd6107f6b1ce8828a167c9e8
David Kornel <dkornel@redhat.com>
2017-05-09 12:50:02 +0200

    NEW: Added tests project

---
+++ ClientUnitTests/SendReceiveTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientUnitTests
+{
+    [TestClass]
+    public class SendReceiveTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
