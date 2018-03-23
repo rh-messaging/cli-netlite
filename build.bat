aecaa203ee3fca9f1368b6054bf1b2357bee5243
David <kornys@outlook.com>
2018-03-23 18:26:33 +0100

    NEW build scripts

---
+++ build.bat
@@ -0,0 +1,13 @@
+rem build complete client
+SET MS_COMMUNITY_PATH="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+SET MS_PRO_PATH="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+if exist %MS_COMMUNITY_PATH% (
+    %MS_COMMUNITY_PATH% cli-netlite.sln /verbosity:minimal
+) else (
+    %MS_PRO_PATH% cli-netlite.sln /verbosity:minimal
+)
+
+rem publish netcore client
+dotnet publish -f netcoreapp2.0 .\NetCoreSender\NetCoreSender.csproj
+dotnet publish -f netcoreapp2.0 .\NetCoreReceiver\NetCoreReceiver.csproj
+dotnet publish -f netcoreapp2.0 .\NetCoreConnector\NetCoreConnector.csproj
---
+++ build.sh
@@ -0,0 +1,14 @@
+#build dotnet core client
+dotnet build -f netcoreapp2.0 ./NetCoreSender/NetCoreSender.csproj
+dotnet build -f netcoreapp2.0 ./NetCoreReceiver/NetCoreReceiver.csproj
+dotnet build -f netcoreapp2.0 ./NetCoreConnector/NetCoreConnector.csproj
+
+dotnet publish -f netcoreapp2.0 ./NetCoreSender/NetCoreSender.csproj
+dotnet publish -f netcoreapp2.0 ./NetCoreReceiver/NetCoreReceiver.csproj
+dotnet publish -f netcoreapp2.0 ./NetCoreConnector/NetCoreConnector.csproj
+
+rm ./dist/netcore -rf
+mkdir ./dist/netcore -p
+cp ./NetCoreSender/bin/Debug/netcoreapp2.0/publish/*.{dll,json} ./dist/netcore
+cp ./NetCoreReceiver/bin/Debug/netcoreapp2.0/publish/*.{dll,json} ./dist/netcore
+cp ./NetCoreConnector/bin/Debug/netcoreapp2.0/publish/*.{dll,json} ./dist/netcore
