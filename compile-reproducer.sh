db26c59364d175243c8621a8f152c112fb3e826f
dstuchli <dstuchli@redhat.com>
2021-03-01 12:21:55 +0100

    FIX removed sensitive URLs from compile script, minor fixes

--- compile-reproducer.sh
+++ compile-reproducer.sh
@@ -22,25 +22,6 @@ fi
 rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
 
-# Search and replace Package AMQPNetLite.Core with AMQP.dll
-sed -i 's/PackageReference Include="AMQPNetLite.Core" Version="2.3.0/Reference Include="AMQP.dll/g' $REPRODUCER_DIR/TcpKeepAliveSettings.csproj
-
-# Download dotnet core client
-if ! wget -q -t 10 "http://download-node-02.eng.bos.redhat.com/devel/candidates/amq/AMQ-CLIENTS-$CLI_VER$CLI_BUILD/amq-clients-$CLI_VER-dotnet-core.zip"; then
-	echo "Could not download dotnet-core client version"
-	exit 1
-fi
-
-# Unzip dotnet core client
-unzip -o amq-clients-$CLI_VER-dotnet-core.zip
-rm -f amq-clients-$CLI_VER-dotnet-core.zip
-
-# Copy AMQP.dll lib into the reproducers dir
-cp amq-clients-$CLI_VER-dotnet-core/bin/netstandard2.0/AMQP.dll $REPRODUCER_DIR
-
-# Delete dotnet core client
-rm -rf amq-clients-$CLI_VER-dotnet-core
-
 # Compile the files
 dotnet build -f netcoreapp3.1 -c Release $REPRODUCER_DIR/TcpKeepAliveSettings.csproj
 
@@ -54,12 +35,4 @@ cp $REPRODUCER_DIR/bin/Release/netcoreapp3.1/TcpKeepAliveSettings $TARGET_DIR/Tc
 cp $REPRODUCER_DIR/bin/Release/netcoreapp3.1/TcpKeepAliveSettings.dll $TARGET_DIR/TcpKeepAliveSettings-reproducer-$CLI_VER
 cp $REPRODUCER_DIR/bin/Release/netcoreapp3.1/TcpKeepAliveSettings.runtimeconfig.json $TARGET_DIR/TcpKeepAliveSettings-reproducer-$CLI_VER
 cd $TARGET_DIR && zip -r TcpKeepAliveSettings-reproducer-$CLI_VER\.zip TcpKeepAliveSettings-reproducer-$CLI_VER
-
-# Generate checksums
-# ARTIFACT_MD5_CHECKSUM=$(md5sum TcpKeepAliveSettings-reproducer-2.9.0.zip | awk '{print $1}')
-# ARTIFACT_SHA1_CHECKSUM=$(shasum -a 1 TcpKeepAliveSettings-reproducer-2.9.0.zip | awk '{ print $1 }')
-
-# Upload to artifactory
-# curl --header "X-Checksum-MD5:${ARTIFACT_MD5_CHECKSUM}" --header "X-Checksum-Sha1:${ARTIFACT_SHA1_CHECKSUM}" -u anonymous: -X PUT "http://messaging-qe-repo.usersys.redhat.com:8081/artifactory/cli-netlite/reproducers/" -T "TcpKeepAliveSettings-reproducer-$CLI_VER.zip"
-curl -u anonymous: -X PUT "http://messaging-qe-repo.usersys.redhat.com:8081/artifactory/cli-netlite/reproducers/" -T "TcpKeepAliveSettings-reproducer-$CLI_VER.zip"
 
