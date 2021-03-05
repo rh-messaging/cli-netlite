96d8024cc2b7682573ecb19366ea9333bbd4ba68
dstuchli <dstuchli@redhat.com>
2021-03-05 14:20:22 +0100

    FIX use MSBuild instead of dotnet

--- build-netlite.sh
+++ build-netlite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 usage() {                                                                                                                  
-        echo "usage: $(basename "$0") [-h|--help] <clients_version> [clients_build]"                  
+        echo "usage: $(basename "$0") [-h|--help] <clients_version>"                  
 }                                                                                                                          
                                                                                                                            
 if [ $# -lt 1 ] || [[ "$@" = *"-h"* ]]; then                                                                               
@@ -10,13 +10,8 @@ if [ $# -lt 1 ] || [[ "$@" = *"-h"* ]]; then
 fi                                                                                                                         
                                                                                                                                                                                                   
 CLI_VER=$1                                                                                                                 
-CLI_BUILD=${2:-""}                                                                                                         
 TARGET_DIR=./dist/cli-netlite-$CLI_VER
                                                                                                                            
-if [[ "$CLI_BUILD" != "" ]]; then                                                                                          
-    CLI_BUILD=".$CLI_BUILD"                                                                                                
-fi                                                                                                                         
-
 # Create directory for finished product
 rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR                                                                                                    
@@ -24,22 +19,11 @@ mkdir -p $TARGET_DIR
 # Install dependency packages
 nuget install src/dotNet/ClientLib/packages.config -OutputDirectory packages
 
-# # Build the projects
-# /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/ClientLib/ClientLib.csproj
-# /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Connector/Connector.csproj
-# /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Receiver/Receiver.csproj
-# /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Sender/Sender.csproj
-
-dotnet build -f netcoreapp3.1 -c Release src/dotNet/ClientLib/ClientLib.csproj
-dotnet build -f netcoreapp3.1 -c Release src/dotNet/Connector/Connector.csproj
-dotnet build -f netcoreapp3.1 -c Release src/dotNet/Receiver/Receiver.csproj
-dotnet build -f netcoreapp3.1 -c Release src/dotNet/Sender/Sender.csproj
-
-# Publish all projects
-dotnet publish -f netcoreapp3.1 -c Release src/dotNet/ClientLib/ClientLib.csproj
-dotnet publish -f netcoreapp3.1 -c Release src/dotNet/Connector/Connector.csproj
-dotnet publish -f netcoreapp3.1 -c Release src/dotNet/Receiver/Receiver.csproj
-dotnet publish -f netcoreapp3.1 -c Release src/dotNet/Sender/Sender.csproj
+# Build the projects
+/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/ClientLib/ClientLib.csproj
+/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Connector/Connector.csproj
+/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Receiver/Receiver.csproj
+/cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/Sender/Sender.csproj
 
 # Create new zip file including all necessary bin files
 cp ./src/dotNet/ClientLib/bin/Release/Amqp.net.dll $TARGET_DIR
