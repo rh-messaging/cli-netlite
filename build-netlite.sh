f6e4ebf417191eb8e5f2c5a28bc5c2e4a5d92269
dstuchli <dstuchli@redhat.com>
2021-09-06 10:58:13 +0200

    FIX: nuget download and package restore with configfile

--- build-netlite.sh
+++ build-netlite.sh
@@ -7,7 +7,8 @@ rm -rf $TARGET_DIR
 mkdir -p $TARGET_DIR
 
 # Restore dependency packages
-/cygdrive/c/tools/nuget.exe restore src/dotNet/ClientLib/packages.config -PackagesDirectory packages
+curl -LO "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+./nuget.exe restore src/dotNet/ClientLib/packages.config -PackagesDirectory packages -ConfigFile NuGet.Config
 
 # Build the projects
 /cygdrive/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe /p:Configuration=Release /p:TargetFrameworkVersion=v4.7.2 src/dotNet/ClientLib/ClientLib.csproj
